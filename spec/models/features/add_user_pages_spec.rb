96ce28d055a5460fe8c1610288428605fe01f3fc
J. TURNER <turner.j6@gmail.com>
2015-03-24 13:33:12 -0700

    Create 'add user' spec.

---
+++ spec/models/features/add_user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "add user" do
+
+  it "adds a new user" do
+    visit users_path
+    click_on 'Create User'
+    fill_in 'Name', :with => 'Charlie Parker'
+    click_on 'Create User'
+    expect(page).to have_content 'Welcome, Beautiful!'
+  end
+
+end
